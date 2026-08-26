a6a9f658fb8505cd308220681a4b3a6e0f051c16
feminellaa <puneetkushwaha750@gmail.com>
2026-08-26 13:39:48 +0530

    Fixed product image paths

---
+++ products.js
@@ -0,0 +1,43 @@
+const products = [
+
+    {
+        cat: "saree",
+        name: "Noir Bandhani Saree",
+        price: "₹1,499",
+        img: "saree-black.png",
+        desc: "Beautiful Bandhani Saree"
+    },
+
+    {
+        cat: "suit",
+        name: "Royal Purple Embroidered Suit",
+        price: "₹1,299",
+        img: "suit-purple.png",
+        desc: "Elegant embroidered suit"
+    },
+
+    {
+        cat: "saree",
+        name: "Designer Saree Collection",
+        price: "₹1,899",
+        img: "saree-collection.png",
+        desc: "Beautiful designer saree"
+    },
+
+    {
+        cat: "kurti",
+        name: "Ivory Printed Kurti Set",
+        price: "₹899",
+        img: "kurti-white.png",
+        desc: "Stylish printed kurti set"
+    },
+
+    {
+        cat: "suit",
+        name: "Ruby Pink Embroidered Suit Set",
+        price: "₹1,599",
+        img: "suit-pink.png",
+        desc: "Beautiful pink embroidered suit"
+    }
+
+];
